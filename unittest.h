fe2f69b1bf4790f5cc3d6b84a2a6990294f0c099
Hans Jorg Andreas Schneebeli <hans.schneebeli@ufes.br>
2019-05-30 16:05:26 -0300

    Using inlines in the unit test package

--- unittest.h
+++ unittest.h
@@ -6,149 +6,122 @@
  *  @brief  routines for unit test
  *
  *  @note   Test for strings uses strcmp. A include<string.h> is mandatory.
- *  @note   Uses C99 declaration of variables inside block.
  *
  *  @author Hans
  *  @date   21/04/2019
  */
 
-/// Prefix for global variables
+int strcmp(char *, char *);
+
 #define TESTPREFIX unit
 
-/// Concatenate operator for C preprocessor
 #define TESTCONCAT(X,Y) X ## Y
 
-/// Names of global variables TESTPREFIXtestXXXXXX
 #define TESTCOUNTER     TESTCONCAT(TESTPREFIX,testCOUNTER)
 #define TESTFAILS       TESTCONCAT(TESTPREFIX,testFAILS)
 #define TESTVERBOSEFLAG TESTCONCAT(TESTPREFIX,testVERBOSE)
 
-/// Global variables
+
 static unsigned TESTCOUNTER     = 0;
 static unsigned TESTFAILS       = 0;
 static unsigned TESTVERBOSEFLAG = 1;
 
-/// Hack to avoid problems in the use of macros in if then else commands
-#define _BEGIN \
-    do {
 
-#define _END \
-    } while(0)
+static inline void  TESTSILENT(void) {
 
-/// Do not output TEXTMESSAGES
-#define TESTSILENT()  \
-    TESTVERBOSEFLAG = 0
+    TESTVERBOSEFLAG = 0;
+}
 
-/// Do output TEXTMESSAGES
-#define TESTVERBOSE() \
-    TESTVERBOSEFLAG = 1
+static inline void  TESTVERBOSE(void) {
 
-/// Initialize global variables. Not necessary now but could be in future versions
-#define TESTINIT() \
-    _BEGIN \
-        TESTCOUNTER = 0;    \
-        TESTFAILS   = 0;    \
-        TESTVERBOSEFLAG = 1;\
-    _END
+    TESTVERBOSEFLAG = 1;
+}
 
-/// Test for int values
-#define TESTINT(EXP,OBT) \
-    _BEGIN \
-        int testEXP = EXP;\
-        int testOBT = OBT; \
-        TESTCOUNTER++;\
-        if( testEXP != testOBT ) { \
-            TESTFAILS++;\
-            fprintf(stderr,"Linha %d File %s: Esperado %d Obtido %d\n",\
-                __LINE__,\
-                __FILE__,\
-                testEXP,\
-                testOBT);\
-        } \
-        \
-    _END
+static inline void  TESTINIT(void) {
+    TESTCOUNTER = 0;
+    TESTFAILS   = 0;
+}
 
-/// Test for bool values
-/// @note Values are inverted to get them in 0 or 1 range
-#define TESTBOOL(EXP,OBT) \
-    _BEGIN \
-        int testEXP = !EXP;\
-        int testOBT = !OBT; \
-        TESTCOUNTER++;\
-        if( testEXP != testOBT ) { \
-            TESTFAILS++;\
-            fprintf(stderr,"Linha %d File %s: Esperado %d Obtido %d\n",\
-                __LINE__,\
-                __FILE__,\
-                !testEXP,\
-                !testOBT);\
-        } \
-        \
-    _END
+static inline void TESTINT(int exp, int obt) {
 
-/// Test for float values.
-/// #note Should use a tolerance! Not done yet
-#define TESTFLOAT(EXP,OBT) \
-    _BEGIN \
-        double testEXP = EXP; \
-        double testOBT = OBT; \
-        TESTCOUNTER++;\
-        if( testEXP != testOBT ) { \
-            TESTFAILS++;\
-            fprintf(stderr,"Linha %d File %s: Esperado %g Obtido %g\n",\
-                __LINE__,\
-                __FILE__,\
-                testEXP,\
-                testOBT);\
-        } \
-        \
-    _END
+    TESTCOUNTER++;
+    if( exp != obt ) {
+        TESTFAILS++;
+        fprintf(stderr,"Linha %d File %s: Esperado %d Obtido %d\n",
+            __LINE__,
+            __FILE__,
+            exp,
+            obt);
+    }
+}
 
-/// Test for string values
-#define TESTSTRING(EXP,OBT) \
-    _BEGIN \
-        char *testEXP = EXP; \
-        char *testOBT = OBT; \
-        TESTCOUNTER++;\
-        if( strcmp(EXP,OBT) == 0 ) { \
-            TESTFAILS++;\
-            fprintf(stderr,"Linha %d File %s: Esperado %s Obtido %s\n",\
-                __LINE__,\
-                __FILE__,\
-                testEXP,\
-                testOBT);\
-        } \
-        \
-    _END
+static inline void TESTBOOL(int exp, int obt) {
 
-/// Test for pointers
-#define TESTPOINTER(EXP,OBT) \
-    _BEGIN \
-        void *testEXP = EXP; \
-        void *testOBT = OBT; \
-        TESTCOUNTER++;\
-        if( testEXP != testOBT ) { \
-            TESTFAILS++;\
-            fprintf(stderr,"Linha %d File %s: Esperado %p Obtido %p\n",\
-                __LINE__,\
-                __FILE__,\
-                testEXP,\
-                testOBT);\
-        } \
-        \
-    _END
+    exp = !exp;     // TRUE != 0, but !TRUE = 0 and !FALSE = 1
+    obt = !obt;
+    TESTCOUNTER++;
+    if( exp != obt ) {
+        TESTFAILS++;
+        fprintf(stderr,"Linha %d File %s: Esperado %d Obtido %d\n",
+            __LINE__,
+            __FILE__,
+            !exp,
+            !obt);
+    }
+}
 
-/// Output a message
-/// @note Can be silented
-#define TESTMESSAGE(S) \
-    _BEGIN \
-        if( TESTVERBOSEFLAG ) fprintf(stderr,"%s\n",S); \
-    _END
+static inline void TESTFLOAT(double exp, double obt) {
 
-/// Print a summary with number of tests and fails
-#define TESTSUMMARY() \
-    fprintf(stderr,"%d tests %d fails\n", \
-                TESTCOUNTER,\
-                TESTFAILS)
+    TESTCOUNTER++;
+    if( exp != obt ) {
+        TESTFAILS++;
+        fprintf(stderr,"Linha %d File %s: Esperado %g Obtido %g\n",
+            __LINE__,
+            __FILE__,
+            exp,
+            obt);
+    }
+}
+
+static inline void TESTSTRING(char *exp, char *obt) {
+
+    TESTCOUNTER++;
+    if( strcmp(exp,obt) == 0 ) {
+        TESTFAILS++;
+        fprintf(stderr,"Linha %d File %s: Esperado %s Obtido %s\n",
+            __LINE__,
+            __FILE__,
+            exp,
+            obt);
+    }
+}
+
+static inline void  TESTPOINTER(void *exp, void *obt) {
+
+    TESTCOUNTER++;
+    if( exp != obt ) {
+        TESTFAILS++;
+        fprintf(stderr,"Linha %d File %s: Esperado %p Obtido %p\n",
+            __LINE__,
+            __FILE__,
+            exp,
+            obt);
+    }
+
+}
+
+
+static inline void TESTMESSAGE(char *s) {
+
+    if( TESTVERBOSEFLAG )
+        fprintf(stderr,"%s\n",s);
+}
+
+static inline void TESTSUMMARY(void) {
+
+    fprintf(stderr,"%d tests %d fails\n",
+        TESTCOUNTER,
+        TESTFAILS);
+}
 
 #endif
